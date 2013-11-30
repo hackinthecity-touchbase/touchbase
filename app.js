4dc2d8f0f482155f9efbdb53833bc29e3295ae30
nicolagreco <notsecurity@gmail.com>
2013-11-30 14:44:42 +0000

    adding all files

---
+++ app.js
@@ -0,0 +1,48 @@
+var express        = require("express");
+var __             = require("underscore");
+var async          = require("async");
+var RedisStore     = require("connect-redis")(express);
+var http           = require('http');
+var util           = require('util');
+var passport       = require("./classes/passport");
+var store          = require('./classes/store').Redis;
+var ejs            = require("ejs");
+var app = module.exports = express();
+
+app.configure(function () {
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "ejs");
+  app.engine("html", ejs.renderFile);
+  app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + "/static/uploads" }));
+  app.use(express.cookieParser("This is the answer you are looking for %&$!$%$"));
+  app.use(express.session({ store: new RedisStore({client: store}) }));
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + "/static"));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+
+var port = process.env.PORT || 1201;
+var server = http.createServer(app).listen(port, function () {
+	console.log("Vera server started on port ", server.address().port, app.settings.env);
+});
+
+var io = require('socket.io').listen(server);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('test', function(data) {
+    console.log('test');
+  });
+});
---
+++ models/user.js
@@ -0,0 +1,40 @@
+var db = require('../classes/store').MongoDB;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  email:  { type: String, required: true }
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, next) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    next(null, isMatch);
+  });
+};
+
+mongoose.model("User", userSchema);
+
+module.exports = {
+  model: db.model("User")
+};
